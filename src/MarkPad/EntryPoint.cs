734c2159706f09c7a266ed9fddbc3e98627160d3
Brendan Forster <me@brendanforster.com>
2012-01-06 21:50:02 +1100

    one class per codefile

---
+++ src/MarkPad/EntryPoint.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace MarkPad
+{
+    public class EntryPoint
+    {
+        [STAThread]
+        public static void Main(string[] args)
+        {
+            SingleInstanceManager manager = new SingleInstanceManager();
+            manager.Run(args);
+        }
+    }
+}
--- src/MarkPad/SingleInstanceManager.cs
+++ src/MarkPad/SingleInstanceManager.cs
@@ -1,23 +1,16 @@
-﻿using System;
+using System.Collections.Generic;
 using System.IO;
 using System.Linq;
 using System.Windows;
 using MarkPad.Shell;
+using StartupEventArgs = Microsoft.VisualBasic.ApplicationServices.StartupEventArgs;
 
 namespace MarkPad
 {
-    public class EntryPoint
-    {
-        [STAThread]
-        public static void Main(string[] args)
-        {
-            SingleInstanceManager manager = new SingleInstanceManager();
-            manager.Run(args);
-        }
-    }
-
     public class SingleInstanceManager : Microsoft.VisualBasic.ApplicationServices.WindowsFormsApplicationBase
     {
+        private IList<string> validExtensions = new[] { ".md", ".markdown", ".mdown" };
+
         public SingleInstanceManager()
         {
             IsSingleInstance = true;
@@ -25,8 +18,37 @@ namespace MarkPad
 
         protected override bool OnStartup(Microsoft.VisualBasic.ApplicationServices.StartupEventArgs eventArgs)
         {
-            Loader.Start();
+            var file = GetFile(eventArgs);
+
+            if (!string.IsNullOrWhiteSpace(file))
+            {
+                Loader.Start(file);
+
+                // Loader.Start with filename
+            }
+            else
+            {
+                Loader.Start();
+            }
+
+           
+
+            
             return false;
+        }
+
+        private string GetFile(StartupEventArgs eventArgs)
+        {
+            var args = eventArgs.CommandLine.ToArray();
+
+            if (args.Length == 1)
+            {
+                var filePath = args[0];
+                if (File.Exists(filePath) && validExtensions.Contains(Path.GetExtension(filePath)))
+                    return filePath;
+            }
+
+            return string.Empty;
         }
 
         protected override void OnStartupNextInstance(Microsoft.VisualBasic.ApplicationServices.StartupNextInstanceEventArgs eventArgs)
