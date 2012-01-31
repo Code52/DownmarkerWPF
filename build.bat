859cc82dd6519446b6b3c4fe80d2febf662c4952
Cameron MacFarland <distantcam@gmail.com>
2012-02-01 07:11:47 +0800

    Removed unused platform types from solution file.

--- build.bat
+++ build.bat
@@ -8,6 +8,6 @@ IF '%1'=='' (SET configuration=Debug) ELSE (SET configuration=%1)
 :: Build the solution. Override the platform to account for running
 :: from Visual Studio Tools command prompt (x64). Log quietly to the 
 :: console and verbosely to a file.
-%msbuild% MarkPad.sln /nologo /property:Configuration=%configuration% /property:Platform=x86 /verbosity:minimal /flp:verbosity=diagnostic
+%msbuild% MarkPad.sln /nologo /property:Configuration=%configuration% /verbosity:minimal /flp:verbosity=diagnostic
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
