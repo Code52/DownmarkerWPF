43bea69ab8471b18d136438dbc080b95c0c97a13
Aaron Powell <me@aaron-powell.com>
2012-02-07 23:05:18 +1100

    Changing to a valid platform

--- build.bat
+++ build.bat
@@ -7,6 +7,6 @@ IF '%1'=='' (SET configuration=Debug) ELSE (SET configuration=%1)
 :: Build the solution. Override the platform to account for running
 :: from Visual Studio Tools command prompt (x64). Log quietly to the 
 :: console and verbosely to a file.
-%msbuild% MarkPad.sln /nologo /property:Platform=x86 /property:Configuration=%configuration% /verbosity:minimal /flp:verbosity=diagnostic
+%msbuild% MarkPad.sln /nologo /property:Platform="Mixed Platforms" /property:Configuration=%configuration% /verbosity:minimal /flp:verbosity=diagnostic
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
