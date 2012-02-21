cbaf19adbd901eca49aae2144fc08aa5d8648a45
aeoth <paul@theleagueofpaul.com>
2012-02-21 19:56:36 +1100

    Properly fixed maximise behaviour

--- src/MarkPad/Shell/ShellView.xaml.cs
+++ src/MarkPad/Shell/ShellView.xaml.cs
@@ -11,9 +11,30 @@ namespace MarkPad.Shell
         private void DragMoveWindow(object sender, MouseButtonEventArgs e)
         {
             if (e.RightButton != MouseButtonState.Pressed && e.MiddleButton != MouseButtonState.Pressed && e.LeftButton == MouseButtonState.Pressed)
-                DragMove();
+            {
+                if (WindowState == WindowState.Maximized)
+                {
+                    // Calcualting correct left coordinate for multi-screen system.
+                    double mouseX = PointToScreen(Mouse.GetPosition(this)).X;
+                    double width = RestoreBounds.Width;
+                    double left = mouseX - width / 2;
 
-            if (e.ClickCount != 2) 
+                    // Aligning window's position to fit the screen.
+                    double virtualScreenWidth = SystemParameters.VirtualScreenWidth;
+                    left = left < 0 ? 0 : left;
+                    left = left + width > virtualScreenWidth ? virtualScreenWidth - width : left;
+
+                    Top = 0;
+                    Left = left;
+
+                    // Restore window to normal state.
+                    WindowState = WindowState.Normal;
+
+                }
+
+                DragMove();
+            }
+            if (e.ClickCount != 2)
                 return;
 
             WindowState = WindowState == WindowState.Maximized ? WindowState.Normal : WindowState.Maximized;
@@ -87,31 +108,6 @@ namespace MarkPad.Shell
         private void DismissCheatSheet(object sender, RoutedEventArgs e)
         {
             ToggleCheatSheet();
-        }
-
-        private void MoveWindow(object sender, MouseEventArgs e)
-        {
-            if (e.RightButton != MouseButtonState.Pressed && e.MiddleButton != MouseButtonState.Pressed
-    && e.LeftButton == MouseButtonState.Pressed && WindowState == WindowState.Maximized)
-            {
-                // Calcualting correct left coordinate for multi-screen system.
-                double mouseX = PointToScreen(Mouse.GetPosition(this)).X;
-                double width = RestoreBounds.Width;
-                double left = mouseX - width / 2;
-
-                // Aligning window's position to fit the screen.
-                double virtualScreenWidth = SystemParameters.VirtualScreenWidth;
-                left = left < 0 ? 0 : left;
-                left = left + width > virtualScreenWidth ? virtualScreenWidth - width : left;
-
-                Top = 0;
-                Left = left;
-
-                // Restore window to normal state.
-                WindowState = WindowState.Normal;
-
-                DragMove();
-            }
         }
     }
 }
