dd6587445e1de9b4a06c8fb3668aafb5cbd507aa
Brendan Forster <me@brendanforster.com>
2012-03-22 11:54:56 +1100

    added testing config and updated build scripts to differentiate between release build and testing build

--- build-Release.bat
+++ build-Release.bat
@@ -1,8 +1,11 @@
 @ECHO OFF
 
 SET build="%~dp0\build.bat"
+SET copy="copy"
 
 :: Pass the configuration parameter and all other parameters
-CALL %build% Release
+CALL %build% Release "Mixed Platforms"
+
+CALL xcopy "%~dp0src\MarkPad\bin\Release\*" "%~dp0artifacts\" /s /e /Y
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
