c7fae5284c3bae27f9998c115edd69f9cc6f5cc9
Ben Scott <ben@belfryimages.com.au>
2012-03-27 08:22:06 +1000

    #179 set up skeleton extension support and extracted spell check

---
+++ src/MarkPad.Services/Extensions/IMarkPadExtension.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace MarkPad.Services.Extensions
+{
+	public interface IMarkPadExtension
+	{
+		string Name { get; }
+	}
+}
---
+++ src/MarkPad/Extensions/IDocumentViewExtension.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using MarkPad.Document;
+
+namespace MarkPad.Extensions
+{
+	public interface IDocumentViewExtension : IMarkPadExtension
+	{
+		void ConnectToDocumentView(DocumentView view);
+		void DisconnectFromDocumentView();
+	}
+}
