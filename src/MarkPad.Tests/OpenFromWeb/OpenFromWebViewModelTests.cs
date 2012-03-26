746b094d51047cf11b10b939b41af320a6773738
Brendan Forster <me@brendanforster.com>
2012-03-26 21:19:28 +1100

    added test project and updated ncrunch configuration

---
+++ src/MarkPad.Tests/OpenFromWeb/OpenFromWebViewModelTests.cs
@@ -0,0 +1,26 @@
+﻿using MarkPad.OpenFromWeb;
+using MarkPad.Services.Interfaces;
+using NSubstitute;
+using Xunit;
+
+namespace MarkPad.Tests.OpenFromWeb
+{
+    public class OpenFromWebViewModelTests
+    {
+        OpenFromWebViewModel subject;
+        
+        readonly IMetaWeblogService metaWeblogService = Substitute.For<IMetaWeblogService>();
+        readonly IDialogService dialogService = Substitute.For<IDialogService>();
+
+        public OpenFromWebViewModelTests()
+        {
+            subject = new OpenFromWebViewModel(dialogService, s => metaWeblogService);
+        }
+
+        [Fact]
+        public void Monkey()
+        {
+            Assert.NotNull(subject);
+        }
+    }
+}
