5363445e36f5d60121c319f04ede491fb32a9cc3
Brendan Forster <me@brendanforster.com>
2012-03-27 14:59:17 +1100

    moved up to Implementation folder

--- src/MarkPad.Services/Implementation/TaskSchedulerFactory.cs
+++ src/MarkPad.Services/Implementation/TaskSchedulerFactory.cs
@@ -1,6 +1,7 @@
 using System.Threading.Tasks;
+using MarkPad.Services.Interfaces;
 
-namespace MarkPad.Services.Interfaces
+namespace MarkPad.Services.Implementation
 {
     public class TaskSchedulerFactory : ITaskSchedulerFactory
     {
