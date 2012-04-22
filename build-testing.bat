1346ce8a6e8a8aef71f2b60b4551b6741b9fc385
Brendan Forster <me@brendanforster.com>
2012-04-22 14:27:15 +1000

    testing build scripts again

--- build-testing.bat
+++ build-testing.bat
@@ -3,9 +3,8 @@
 SET build="%~dp0\build.bat"
 SET xunit="%~dp0\tools\xunit\xunit.console.clr4.exe"
 
-:: Pass the configuration parameter and all other parameters
 CALL %build% Testing "Any CPU"
 
-::CALL %xunit% "%~dp0src\MarkPad.Tests\bin\Testing\MarkPad.Tests.dll"
+CALL %xunit% "%~dp0src\MarkPad.Tests\bin\Testing\MarkPad.Tests.dll"
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
