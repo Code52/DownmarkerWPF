4b5c619a68d0dd326a73cf9a4a9b79c7f023f408
Brendan Forster <me@brendanforster.com>
2012-04-22 15:59:19 +1000

    updated packages and incremented version number

---
+++ src/MarkPad.XAML/Analects/XAMLConverters/ToUpperConverter.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Analects.XAMLConverters
+{
+    public class ToUpperConverter : MarkupConverter
+    {
+        protected override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            if (value is string)
+                return ((string)value).ToUpper(culture);
+
+            return value;
+        }
+
+        protected override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return value;
+        }
+    }
+}
