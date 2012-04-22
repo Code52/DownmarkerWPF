554b33f95d32ac9e34f9e749c2574ba0b081ddc8
Brendan Forster <me@brendanforster.com>
2012-04-22 13:40:40 +1000

    updated version correctly for setup

--- src/GlobalAssemblyInfo.cs
+++ src/GlobalAssemblyInfo.cs
@@ -1,10 +1,10 @@
 using System.Reflection;
 
-[assembly: AssemblyConfiguration("BETA")]
+[assembly: AssemblyConfiguration("RC")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 [assembly: AssemblyCompany("Code52")]
 [assembly: AssemblyCopyright("Copyright © Code52")]
 
-[assembly: AssemblyVersion("0.9")]
-[assembly: AssemblyFileVersion("0.9")]
+[assembly: AssemblyVersion("0.9.0.1")]
+[assembly: AssemblyFileVersion("0.9.0.1")]
