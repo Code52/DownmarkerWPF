6a1ea68320251423f423c0fb848e641ef20f1262
Jake Ginnivan <jake.ginnivan@readify.net>
2012-05-05 14:34:11 +0800

    Added FunnelWeb style Rsd test

--- src/MarkPad.Tests/Services/RsdServiceTests.cs
+++ src/MarkPad.Tests/Services/RsdServiceTests.cs
@@ -29,6 +29,31 @@ namespace MarkPad.Tests.Services
             Assert.Equal("https://www.codeplex.com/site/metaweblog", result.MetaWebLogApiLink);
         }
 
+        [Fact]
+        public void discovers_site_with_rsdxml_file_in_root()
+        {
+            // arrange
+            webRequestFactory.RegisterResultForUri("http://funnelweblog.net/rsd.xml", FunnelWebRsdFile);
+
+            // act
+            var result = rsdService.DiscoverAddress("http://funnelweblog.net").Result;
+
+            // assert
+            Assert.True(result.Success);
+            Assert.Equal("http://funnelweblog.net/blogapi", result.MetaWebLogApiLink);
+        }
+
+        private const string FunnelWebRsdFile = @"<rsd version=""1.0"" xmlns=""http://archipelago.phrasewise.com/rsd"">
+  <service>
+    <engineName>FunnelWeblog</engineName>
+    <engineLink>http://www.funnelweblog.com/</engineLink>
+    <homePageLink>http://funnelweblog.net/</homePageLink>
+    <apis>
+      <api name=""MetaWeblog"" preferred=""true"" apiLink=""http://funnelweblog.net/blogapi"" blogId=""something"" />
+    </apis>
+  </service>
+</rsd>";
+
         private const string CodeplexProjectPage = @"<!DOCTYPE html><html xmlns=""http://www.w3.org/1999/xhtml"" xml:lang=""en"" lang=""en"">
 <head>
 <link rel=""EditURI"" type=""application/rsd+xml"" title=""RSD"" href='http://project.codeplex.com/rsd' /></head><body></body></html>";
