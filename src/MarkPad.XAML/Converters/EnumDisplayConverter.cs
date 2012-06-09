ef516db322a8f203f5a8c46a0bfe7e84e137839d
Jake Ginnivan <jake.ginnivan@readify.net>
2012-06-10 05:15:26 +0800

    Replaced DisplayStringAttribute with build in DescriptionAttribute

--- src/MarkPad.XAML/Converters/EnumDisplayConverter.cs
+++ src/MarkPad.XAML/Converters/EnumDisplayConverter.cs
@@ -1,15 +1,16 @@
 using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
 using System.Reflection;
-using System.Resources;
 using Analects.XAMLConverters;
 
 namespace MarkPad.XAML.Converters
 {
     public class EnumDisplayConverter : MarkupConverter
     {
-        private static readonly Dictionary<object, string> displayValues = new Dictionary<object, string>();
-        private static readonly Dictionary<string, object> reverseValues = new Dictionary<string, object>();
+        static readonly Dictionary<object, string> DisplayValues = new Dictionary<object, string>();
+        static readonly Dictionary<string, object> ReverseValues = new Dictionary<string, object>();
 
         private void GetDisplayString(object value)
         {
@@ -18,49 +19,42 @@ namespace MarkPad.XAML.Converters
             var fields = valueType.GetFields(BindingFlags.Public | BindingFlags.Static);
             foreach (var field in fields)
             {
-                var a = (DisplayStringAttribute[]) field.GetCustomAttributes(typeof(DisplayStringAttribute), false);
+                var a = (DescriptionAttribute[]) field.GetCustomAttributes(typeof(DescriptionAttribute), false);
 
-                string displayString = GetDisplayStringValue(a, valueType);
-                object enumValue = field.GetValue(null);
+                var displayString = GetDisplayStringValue(a);
+                var enumValue = field.GetValue(null);
 
                 if (displayString == null)
                 {
                     displayString = enumValue.ToString();
                 }
-                if (displayString != null)
-                {
-                    displayValues.Add(enumValue, displayString);
-                    reverseValues.Add(displayString, enumValue);
-                }
+
+                DisplayValues.Add(enumValue, displayString);
+                ReverseValues.Add(displayString, enumValue);
             }
         }
 
-        private string GetDisplayStringValue(DisplayStringAttribute[] a, Type type)
+        static string GetDisplayStringValue(ICollection<DescriptionAttribute> descriptionAttributes)
         {
-            if (a == null || a.Length == 0) return null;
-            DisplayStringAttribute dsa = a[0];
-            if (!string.IsNullOrEmpty(dsa.ResourceKey))
-            {
-                ResourceManager rm = new ResourceManager(type);
-                return rm.GetString(dsa.ResourceKey);
-            }
-            return dsa.Value;
+            if (descriptionAttributes == null || descriptionAttributes.Count == 0) 
+                return null;
+            return descriptionAttributes.First().Description;
         }
 
         protected override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (!displayValues.ContainsKey(value))
+            if (!DisplayValues.ContainsKey(value))
                 GetDisplayString(value);
 
-            return displayValues[value];
+            return DisplayValues[value];
         }
 
         protected override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
         {
-            if (!reverseValues.ContainsKey(value.ToString()))
+            if (!ReverseValues.ContainsKey(value.ToString()))
                 return null;
 
-            return reverseValues[value.ToString()];
+            return ReverseValues[value.ToString()];
         }
     }
 }
