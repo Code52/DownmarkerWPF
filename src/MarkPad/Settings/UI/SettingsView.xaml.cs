a52272dbf948986ecaf7c9ee1cfa0a7ccd701056
Jake Ginnivan <jake.ginnivan@readify.net>
2012-06-22 19:37:43 +0800

    Made font settings readable, pushed preview up 2px so it lines up

--- src/MarkPad/Settings/UI/SettingsView.xaml.cs
+++ src/MarkPad/Settings/UI/SettingsView.xaml.cs
@@ -1,10 +1,20 @@
-﻿namespace MarkPad.Settings.UI
+﻿using System.Windows;
+using System.Windows.Controls;
+
+namespace MarkPad.Settings.UI
 {
     public partial class SettingsView
     {
         public SettingsView()
         {
             InitializeComponent();
+        }
+
+        void ScrollToSelection(object sender, RoutedEventArgs e)
+        {
+            var listBox = (ListBox) sender;
+
+            listBox.ScrollIntoView(listBox.SelectedItem);
         }
     }
 }
