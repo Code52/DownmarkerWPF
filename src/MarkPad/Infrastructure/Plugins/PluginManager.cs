7be3668fecae7fe168a79d0759c20004ada89854
Brendan Forster <me@brendanforster.com>
2012-07-16 22:04:45 +1000

    updated PluginManager to only use assemblies it required

--- src/MarkPad/Infrastructure/Plugins/PluginManager.cs
+++ src/MarkPad/Infrastructure/Plugins/PluginManager.cs
@@ -1,7 +1,9 @@
 using System.Collections.Generic;
 using System.ComponentModel.Composition;
 using System.ComponentModel.Composition.Hosting;
+using System.Reflection;
 using MarkPad.Contracts;
+using MarkPad.Document.SpellCheck;
 using MarkPad.Plugins;
 
 namespace MarkPad.Infrastructure.Plugins
@@ -14,19 +16,17 @@ namespace MarkPad.Infrastructure.Plugins
 
 		public PluginManager()
 		{
-			var catalog = new AggregateCatalog();
-
-			catalog.Catalogs.Add(new AssemblyCatalog(System.Reflection.Assembly.GetExecutingAssembly()));
-			catalog.Catalogs.Add(new AssemblyCatalog(typeof(IPlugin).Assembly));
-			catalog.Catalogs.Add(new AssemblyCatalog(typeof(IDocumentView).Assembly));
-			catalog.Catalogs.Add(new AssemblyCatalog(typeof(PluginSettingsProvider).Assembly));
-
-            //catalog.Catalogs.Add(new AssemblyCatalog(typeof(ExamplePlugin.ExamplePlugin).Assembly));
-            //catalog.Catalogs.Add(new AssemblyCatalog(typeof(ExportToHtmlPlugin.ExportToHtmlPlugin).Assembly));
-            //catalog.Catalogs.Add(new AssemblyCatalog(typeof(SpellCheckPlugin.SpellCheckPlugin).Assembly));
-
+			var catalog = 
+                new AggregateCatalog(
+		            new AssemblyCatalog(typeof (IPlugin).Assembly),
+		            new AssemblyCatalog(typeof (IDocumentView).Assembly),
+		            new AssemblyCatalog(typeof (PluginSettingsProvider).Assembly),
+		            new AssemblyCatalog(typeof (SpellCheckPlugin.SpellCheckPlugin).Assembly));
+            
 			Container = new CompositionContainer(catalog);
 			Container.ComposeParts(this);
+
+            var exports = Container.GetExportedValues<IDocumentViewPlugin>();
 		}
 	}
 }
