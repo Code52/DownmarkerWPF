2c7299e3316595fdeabb96ab5228e02a04f0bb3c
Paul <paul@jenkinsfamily.com.au>
2012-07-29 13:04:14 +1000

    Moved word count and zoom to their own bar, ala Office 2013, to remove overlap

---
+++ src/MarkPad/Converters/NullToVisibilityConverter.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Windows;
+using MahApps.Metro.Converters;
+
+namespace MarkPad.Converters
+{
+    public class NullToVisibilityConverter : MarkupConverter
+    {
+        protected override object Convert(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return (value != null) ? Visibility.Visible : Visibility.Collapsed;
+        }
+        protected override object ConvertBack(object value, Type targetType, object parameter, System.Globalization.CultureInfo culture)
+        {
+            return null;
+        }
+    }
+}
