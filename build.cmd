06c65ab2513217c71aad5fae88076103ceae68aa
Brendan Forster <me@brendanforster.com>
2012-09-02 15:17:08 +1000

    build script tweaks

--- build.cmd
+++ build.cmd
@@ -8,6 +8,6 @@ IF '%2'=='' (SET platform="x86") ELSE (SET platform=%2)
 :: Build the solution. Override the platform to account for running
 :: from Visual Studio Tools command prompt (x64). Log quietly to the 
 :: console and verbosely to a file.
-%msbuild% src/MarkPad.sln /nologo /property:Platform=%platform% /property:Configuration=%configuration% /verbosity:minimal /flp:verbosity=diagnostic
+%msbuild% src/MarkPad.sln /t:Rebuild /nologo /property:Platform=%platform% /property:Configuration=%configuration% /verbosity:minimal /flp:verbosity=diagnostic
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
--- run-tests.cmd
+++ run-tests.cmd
@@ -3,8 +3,8 @@
 SET build="%~dp0\build.cmd"
 SET xunit="%~dp0\tools\xunit\xunit.console.clr4.x86.exe"
 
-CALL %build% Testing "x86"
+CALL %build% Release "x86"
 
-CALL %xunit% "%~dp0src\MarkPad.Tests\bin\Testing\MarkPad.Tests.dll"
+CALL %xunit% "%~dp0src\MarkPad.Tests\bin\Release\MarkPad.Tests.dll"
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
