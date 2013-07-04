38dd910171deacfcfd5b182b1dfddda5f49ac59b
Cameron MacFarland <distantcam@gmail.com>
2013-07-04 14:39:44 +0800

    Proper versioning of installer

--- src/MarkPad/Properties/AssemblyInfo.cs
+++ src/MarkPad/Properties/AssemblyInfo.cs
@@ -2,23 +2,19 @@ using System.Reflection;
 using System.Runtime.InteropServices;
 using System.Windows;
 
-// General Information about an assembly is controlled through the following 
+// General Information about an assembly is controlled through the following
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
 [assembly: AssemblyTitle("MarkPad")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyProduct("MarkPad")]
-[assembly: AssemblyCompany("Code52")]
-[assembly: AssemblyCopyright("Copyright © Code52 2013")]
-[assembly: AssemblyVersion("0.0.0.0")]
-[assembly: AssemblyFileVersion("0.0.0.0")]
 
-// Setting ComVisible to false makes the types in this assembly not visible 
-// to COM components.  If you need to access a type in this assembly from 
+// Setting ComVisible to false makes the types in this assembly not visible
+// to COM components.  If you need to access a type in this assembly from
 // COM, set the ComVisible attribute to true on that type.
 [assembly: ComVisible(false)]
 
-//In order to begin building localizable applications, set 
+//In order to begin building localizable applications, set
 //<UICulture>CultureYouAreCodingWith</UICulture> in your .csproj file
 //inside a <PropertyGroup>.  For example, if you are using US english
 //in your source files, set the <UICulture> to en-US.  Then uncomment
@@ -26,13 +22,11 @@ using System.Windows;
 //the line below to match the UICulture setting in the project file.
 
 //[assembly: NeutralResourcesLanguage("en-US", UltimateResourceFallbackLocation.Satellite)]
-
-
 [assembly: ThemeInfo(
     ResourceDictionaryLocation.None, //where theme specific resource dictionaries are located
-    //(used if a resource is not found in the page, 
+    //(used if a resource is not found in the page,
     // or application resource dictionaries)
     ResourceDictionaryLocation.SourceAssembly //where the generic resource dictionary is located
-    //(used if a resource is not found in the page, 
+    //(used if a resource is not found in the page,
     // app, or any theme specific resource dictionaries)
 )]
