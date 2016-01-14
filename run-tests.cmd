e2249a5b3a4bae4b088fc3b67f1173629da439ef
Brendan Forster <brendan@github.com>
2016-01-14 18:25:36 +1100

    also run the UI tests

--- run-tests.cmd
+++ run-tests.cmd
@@ -6,5 +6,6 @@ SET xunit="%~dp0\tools\xunit\xunit.console.clr4.x86.exe"
 CALL %build% Release "x86"
 
 CALL %xunit% "%~dp0src\MarkPad.Tests\bin\Release\MarkPad.Tests.dll"
+CALL %xunit% "%~dp0src\MarkPad.UITests\bin\Release\MarkPad.UITests.dll"
 
 IF NOT ERRORLEVEL 0 EXIT /B %ERRORLEVEL%
